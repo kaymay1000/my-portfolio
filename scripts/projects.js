472f72e4e81f4eef1fcd56552ab2f2e702ebbcd2
Kaylee Alvarado <kaymay1000@gmail.com>
2017-02-12 09:12:06 -0800

    All technical reqs met for assn02

--- scripts/projects.js
+++ scripts/projects.js
@@ -4,21 +4,21 @@ var rawData = [
   {
     title: 'Campfire Coffee',
     fileSource: 'https://kaymay1000.github.io/campfire_coffee/',
-    img: '/imgs/coffee.jpg',
+    img: '../imgs/coffee.jpg',
     dateCreated: '2016-08-15',
     difficulty: 'advanced'
   },
   {
     title: 'Bus Mall',
     fileSource: 'https://github.com/kaymay1000/bus_mall',
-    img: '/imgs/bus.jpg',
+    img: '../imgs/bus.jpg',
     dateCreated: '2016-09-20',
     difficulty: 'intermediate'
   },
   {
     title: 'Guessing Game',
     fileSource: 'https://kaymay1000.github.io/guessing_game/',
-    img: '/imgs/question.jpg',
+    img: '../imgs/question.jpg',
     dateCreated: '2016-07-14',
     difficulty: 'easy'
   }
