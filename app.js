938d0f054739af903325a93acec27c2e1bc04e84
Kaylee Alvarado <kaymay1000@gmail.com>
2017-02-11 18:08:29 -0800

    Projects rendering (all but images)

--- app.js
+++
@@ -1,9 +0,0 @@
-var projects = [];
-
-function Project(title, dateCreated, difficulty, fileSource) {
-  this.title = title;
-  this.dateCreated = dateCreated;
-  this.difficulty = difficulty;
-  this.fileSource = fileSource;
-  projects.push(this);
-}
