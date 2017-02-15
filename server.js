ef7425bdbfbb8e6699341f868f862040f3b2cfa2
Kaylee Alvarado <kaymay1000@gmail.com>
2017-02-15 11:45:53 -0800

    Added response and request parameters to app.get callback functions

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static('./public'));
 
-app.get('/index.html', function() {
-  app.sendFile('public/index.html', {root: '.'})
+app.get('/index.html', function(request, response) {
+  response.sendFile('public/index.html', {root: '.'})
 })
 
-app.get('*', function() {
-  app.send('Oh no! A 404! Please use a valid route.');
+app.get('*', function(request, response) {
+  response.send('Oh no! A 404! Please use a valid route.');
 })
 
 app.listen(PORT, function() {
