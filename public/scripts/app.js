624defe0446524182b03ed7cf1b3506cb8053883
Kaylee Alvarado <kaymay1000@gmail.com>
2017-02-22 12:50:30 -0800

    Working on handleDiffFilter()

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,43 +2,43 @@
 
 (function(module){
 
-function Project(obj) {
-  this.title = obj.title;
-  this.fileSource = obj.fileSource;
-  this.img = obj.img;
-  this.dateCreated = obj.dateCreated;
-  this.difficulty = obj.difficulty;
-}
-
-Project.all = [];
-
-Project.prototype.toHtml = function() { //functions that are available to (and should apply to) individual Project instances
-  var htmlTemp = $('#handlebarsTemplate').text();
-  var compiledTemp = Handlebars.compile(htmlTemp); //Handlebars.compile() returns a function, so we're able to pass it an argument (this) in the next line.
-  return compiledTemp(this);
-}
-
-Project.loadAll = function(rawData) {
-  rawData.forEach(function(p) {
-    console.log('making project');
-    Project.all.push(new Project(p));
-  });
-}
-
-Project.fetchAll = function() {
-  if (localStorage.jsonProjects) {
-    Project.loadAll(JSON.parse(localStorage.jsonProjects));
-    projectView.appendProject();
-    console.log('fetched from localStorage');
-  } else {
-    let $jsonProjects = $.getJSON('../data/projects.json', function(data) {
-      localStorage.setItem('jsonProjects', JSON.stringify(data)); //set stringified JSON in localStorage to prevent need for server call next time
-      Project.loadAll(data);
-      projectView.appendProject();
-      console.log('fetched from JSON');
-    })
+  function Project(obj) {
+    this.title = obj.title;
+    this.fileSource = obj.fileSource;
+    this.img = obj.img;
+    this.dateCreated = obj.dateCreated;
+    this.difficulty = obj.difficulty;
   }
-}
 
-module.Project = Project;
+  Project.all = [];
+
+  Project.prototype.toHtml = function() { //functions that are available to (and should apply to) individual Project instances
+    var htmlTemp = $('#handlebarsTemplate').text();
+    var compiledTemp = Handlebars.compile(htmlTemp); //Handlebars.compile() returns a function, so we're able to pass it an argument (this) in the next line.
+    return compiledTemp(this);
+  }
+
+  Project.loadAll = function(rawData) {
+    rawData.forEach(function(p) {
+      console.log('making project');
+      Project.all.push(new Project(p));
+    });
+  }
+
+  Project.fetchAll = function() {
+    if (localStorage.jsonProjects) {
+      Project.loadAll(JSON.parse(localStorage.jsonProjects));
+      projectView.appendProject();
+      console.log('fetched from localStorage');
+    } else {
+      let $jsonProjects = $.getJSON('../data/projects.json', function(data) {
+        localStorage.setItem('jsonProjects', JSON.stringify(data)); //set stringified JSON in localStorage to prevent need for server call next time
+        Project.loadAll(data);
+        projectView.appendProject();
+        console.log('fetched from JSON');
+      })
+    }
+  }
+
+  module.Project = Project;
 })(window);
--- public/scripts/projectView.js
+++ public/scripts/projectView.js
@@ -1,23 +1,38 @@
 'use strict';
 (function(module){
 
-const projectView = {};
+  const projectView = {};
 
-projectView.handleMainNav = function() {
-  $('#mainNav').on('click', '.tab', function() {
-    $('.tabContent').hide();
-    $('#' + $(this).data('content')).fadeIn(500);
-  });
-  $('#mainNav .tab:first').click(); //simulate click on Home tab
-}
+  projectView.handleMainNav = function() {
+    $('#mainNav').on('click', '.tab', function() {
+      $('.tabContent').hide();
+      $('#' + $(this).data('content')).fadeIn(500);
+    });
+    $('#mainNav .tab:first').click(); //simulate click on Home tab
+  }
 
-projectView.appendProject = function() {
-  Project.all.forEach(function(currProject) {
-    $('#projects').append(currProject.toHtml());
-  });
-}
+  projectView.appendProject = function() {
+    Project.all.forEach(function(currProject) {
+      $('#projects').append(currProject.toHtml());
+    });
+  }
 
-projectView.handleMainNav();
+  projectView.handleDiffFilter = function() {
+    $('#diffFilter').on('change', function(){
+      $('#projects').hide();
+      var selected = $(this).val();
+      console.log(selected);
+      const filteredProjects = Project.all.filter(function(currProject) {
+        if (currProject.difficulty === selected) {
+          console.log('currProject', currProject)
+          $('#projects').append(currProject);
+        }
+      });
+    });
+    $('#diffFilter').val('default');
+  }
 
-module.projectView = projectView;
+  projectView.handleMainNav();
+  projectView.handleDiffFilter();
+  module.projectView = projectView;
 })(window);
